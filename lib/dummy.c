93e1748d76816eb1d65e1ccee7ca4955359ad130
Samir Ahmed <samirahmed2013@gmail.com>
2013-01-25 13:11:50 -0500

    Added Necessary lib/client/server folders
    From Part 3c) and 5a)

---
+++ lib/dummy.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+void
+dummy_hello(void)
+{
+printf("dummy_hello(): Hello from lib\n");
+}
---
+++ lib/dummy.h
@@ -0,0 +1,5 @@
+#ifndef __BOGOSORT_DUMMY_H__
+#define __BOGOSORT_DUMMY_H__
+void dummy_hello(void);
+#endif
+
