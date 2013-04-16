aaecfb61420446944ab8cbec25014e607bb2b2c1
Katsu K <kkawa1570@gmail.com>
2013-04-16 08:40:59 -0400

    Added pthread function wrappers

--- lib/game_client.c
+++ lib/game_client.c
@@ -8,30 +8,36 @@
 #include "net.h"
 #include "protocol.h"
 #include "protocol_session.h"
-#include "game_server.h"
+#include "game_client.h"
 
-extern int blocking_helper_init(Blocking_Helper *bh)
+extern void blocking_helper_init(Blocking_Helper *bh)
 {
-	pthread_mutex_init(&bh->maze_lock,NULL);
-	pthread_cond_init(&bh->maze_updated,NULL);
+    pthread_mutex_init(&bh->maze_lock,NULL);
+    pthread_cond_init(&bh->maze_updated,NULL);
+}
+
+extern void blocking_helper_destroy(Blocking_Helper *bh)
+{
+    pthread_mutex_destroy(&bh->maze_lock);
+    pthread_cond_destroy(&bh->maze_updated);    
 }
 
 extern void client_maze_lock(Blocking_Helper *bh)
 {
-	pthread_mutex_lock(&bh->maze_lock);
+    pthread_mutex_lock(&bh->maze_lock);
 }
 
 extern void client_maze_unlock(Blocking_Helper *bh)
 {
-	pthread_mutex_lock(&bh->maze_lock);
+    pthread_mutex_lock(&bh->maze_lock);
 }
 
 extern void client_maze_signal(Blocking_Helper *bh)
 {
-	pthread_cond_signal(&bh->maze_updated);
+    pthread_cond_signal(&bh->maze_updated);
 }
 
 extern void client_maze_cond_wait(Blocking_Helper *bh)
 {
-	pthread_cond_wait(&bh->maze_updated,&bh->maze_lock);
+    pthread_cond_wait(&bh->maze_updated,&bh->maze_lock);
 }
