c8043f8ff7a51f4e651314949c406bbab5c4d4f2
Katsu K <kkawa1570@gmail.com>
2013-04-22 12:30:37 -0400

    My test is seg faulting

--- test/client-test.c
+++ test/client-test.c
@@ -5,12 +5,56 @@
 #include <poll.h>
 #include <pthread.h>
 #include <string.h>
+#include <arpa/inet.h>
 #include "../lib/types.h"
 #include "../lib/protocol.h"
+#include "../lib/protocol_client.c"
 #include "../lib/net.h"
 #include "../lib/game_commons.h"
 #include "../lib/game_client.h"
 #include "../lib/test.h"
+
+int test_marshall_int_read_buffer(Proto_Session *s, int v)
+{
+  if (s && ((s->rlen + sizeof(int)) < PROTO_SESSION_BUF_SIZE)) {
+    *((int *)(s->rbuf + s->rlen)) = htonl(v);
+    s->rlen+=sizeof(int);
+    return 1;
+  }
+  return -1;
+}
+
+void test_decompress_sync_client(TestContext* tc)
+{
+    int ii,assertion;
+    char buffer[20];
+    char output[100];
+    //Init Client and Map
+    Client C;
+    client_init(&C);
+    //Compress random things
+    Proto_Session* s;
+    Proto_Client *proto_client = C.ph;
+    s = &(proto_client->rpc_session);
+    for(ii = 0;ii<20;ii++)
+    {
+        test_marshall_int_read_buffer(s,ii);
+    }
+
+    //Get the compress
+    int result;
+    int offset;
+    offset = 0;
+    for(ii = 0;ii<20;ii++)
+    {
+        offset = get_compress_from_body(C.ph,offset,1,&result);
+        assertion = ((result)==ii);
+        sprintf(buffer,"%d",ii);
+        strcat(output,"The result should be ");
+        strcat(output,buffer);
+        should(output,assertion,tc);
+    } 
+}
 
 void st_client_wait_for_event(Task* task)
 {
@@ -54,7 +98,8 @@ int main(int argc, char ** argv )
     
     
     // ADD TESTS HERE
-    run(&test_blocking_threads,"Blocking Threads",&tc);
+//    run(&test_blocking_threads,"Blocking Threads",&tc);
+    run(&test_decompress_sync_client,"Decompressing Body",&tc);
     // TEST END HERE
 
     test_summary(&tc);
