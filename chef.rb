38fa2f9ee80ec1151d3b3b3f695cb98b00e84cb3
Samir Ahmed <samirahmed2013@gmail.com>
2013-04-26 13:39:17 -0400

    recipe builder console
    chef.rb now has a recipe building console.
    
    Other changes include issuing quit commands to the server and clients
    after execution and waiting for all processes to finish.

--- chef.rb
+++ chef.rb
@@ -27,18 +27,28 @@ class String
   end
 end
 
+## Logging helper
+def log(id,cmdstr,verbose,fd)
+  time = Time.now.to_s
+  puts "#{id}".pink+"\t| #{time}"+"\t| #{cmdstr}".yellow if (verbose)
+  fd.write("#{id}\t| #{time}\t| #{cmdstr}\n")
+end
+
+## Command parser
 def mkcmd(cmd,arg)
   cmd.strip if cmd
   arg.strip if arg
   case cmd.strip
     when "HEL" then "hello"
     when "BYE" then "disconnect"
+    when "CDP" then "clientdump"
+    when "SDP" then "serverdump"
     when "MOV" then
       case arg.strip
         when "0" then "move up"
         when "1" then "move right"
-        when "2" then "move left"
-        when "3" then "move down"
+        when "2" then "move down"
+        when "3" then "move left"
         else nil
       end
     when "DRP" then
@@ -57,106 +67,164 @@ def mkcmd(cmd,arg)
   end
 end
 
-# setup paths
-client  = File.join(FileUtils.pwd,'client/client')
-server  = File.join(FileUtils.pwd,'server/server')
+def console()
+  puts "Welcome to the chef console"
+  puts "How many clients? ".yellow
+  clients = $stdin.gets.strip.to_i
+  puts "How many moves per client?".yellow
+  moves =  $stdin.gets.strip.to_i
+  puts "min delay between requests?".yellow
+  mindelay = $stdin.gets.strip.to_i
+  puts "max delay between requests?".yellow
+  maxdelay = $stdin.gets.strip.to_i
+  puts "What do you want to call this recipe?".yellow
+  name = $stdin.gets.strip
+  
+  fname = File.join(FileUtils.pwd,"test/#{name}.recipe")
+  
+  if File.exists?(fname)
+    puts "#{name} already exists! Overwrite it [y/n]?".red
+    ow = $stdin.gets.strip.downcase[0] == "y"
+    return unless ow
+  end
+  
+  delay = lambda{ return (rand(maxdelay-mindelay)+mindelay)/1000.0 }
+  cids = (0...clients).to_a
+  fd = File.open(fname,'w');
 
-# check if client/server exist
-abort("Error! No client found".red) unless File.exists?(client)
-abort("Error! No server found".red) unless File.exists?(server)
-
-ip = 'localhost'
-port, recipe = nil,nil
-pids,fds = [],[]
-debug,client_only, checksum = false,false,false
-
-ARGV.each do |arg|
-  if arg == "--checksum" || arg == "-x"
-    dump = true
-  elsif arg == "--client" || arg == "-c"
-    client_only = true 
-  elsif (arg == "--debug" || arg == "-d")
-    debug = true 
-  elsif (iparg = arg.scan(/(?:[0-9]{1,3}\.){3}[0-9]{1,3}/).first)
-    ip = iparg
-  elsif (portarg = arg.scan(/\d{4,5}/).first)
-    port = portarg
-  else
-    recipe = arg
-  end 
+  fd.puts "#{clients}";
+  cids.shuffle.each{|id| fd.puts "#{id},\t#{delay.call()},\tHEL"}
+  (cids*moves).shuffle.each{|id| fd.puts "#{id},\t#{delay.call()},\tMOV,\t#{rand(4)}"}
+  cids.shuffle.each{|id| fd.puts "#{id},\t#{delay.call()},\tBYE"}
+  fd.close
+  puts "Recipe Saved to test/#{name}.recipe".green
 end
 
-abort("Error! No test recipe specified".red) unless recipe
-abort("Error! Can't find recipe ".red) unless File.exists?(recipe)
+def test()
+  # setup paths
+  client  = File.join(FileUtils.pwd,'client/client')
+  server  = File.join(FileUtils.pwd,'server/server')
 
-# prepare log files
-log_dir = File.join(FileUtils.pwd,'log/')
-slog    = File.join(log_dir,File.basename(recipe,".*")+'-server.log');
-clog    = File.join(log_dir,File.basename(recipe,".*")+'-client.log');
-tlog    = File.join(log_dir,File.basename(recipe,".*")+'-test.log');
+  # check if client/server exist
+  abort("Error! Executable client/client missing".red) unless File.exists?(client)
+  abort("Error! Executable server/server missing".red) unless File.exists?(server)
 
-begin
+  ip = 'localhost'
+  port, recipe = nil,nil
+  pids,fds = [],[]
+  debug,client_only,verbose, checksum = false,false,false,false
 
-  trap("INT"){ puts "Hold your horses".yellow; raise "SIGINT";}
-
-  # start a server
-  if not client_only
-    fd  =  File.open(slog,'w')
-    pids << Process.spawn(server, :err=>fd , :out=>fd )
-    fds  << fd
-     
-    puts "Launching Local Server pid=#{pids.first}"
-    sleep 0.5
-    
-    puts cmd if (debug) 
-    port_str = File.readlines("#{slog}").join("\n").scan(/RPC Port: \d{4,5}/).first;
-    port = port_str.scan(/\d+/).first if port_str
-    raise "Error! Can't parse server log: #{port_str}".red unless port && port_str
+  ARGV.each do |arg|
+    if  arg == "--verbose" || arg == "-v"
+      verbose = true
+    elsif arg == "--checksum" || arg == "-x"
+      checksum = true
+    elsif arg == "--client" || arg == "-c"
+      client_only = true 
+    elsif (arg == "--debug" || arg == "-d")
+      debug = true 
+    elsif (iparg = arg.scan(/(?:[0-9]{1,3}\.){3}[0-9]{1,3}/).first)
+      ip = iparg
+    elsif (portarg = arg.scan(/\d{4,5}/).first)
+      port = portarg
+    else
+      recipe = arg
+    end 
   end
- 
-  # read the port
-  raise "Error! Can't find port".red unless port
-  puts "Server on #{ip}:#{port}"
-  
-  # read the recipe file
-  instructions = CSV.readlines(recipe);
-  count = instructions.shift.first.to_i;
-  # create clients
-  ios = count.times.map{ IO.pipe }
-  ios.each do |io|
-    fd = File.open(clog,'w');
-    pids << Process.spawn("#{client}",:in=>io.first,:err=>fd,:out=>fd)
-    fds << fd
-  end
-  puts "Clients #{pids[1..-1].inspect}"
-  clients = ios.map{|io| io.last}
-  
-  # connect all clients 
-  clients.each{|stdin| stdin.puts "connect #{ip}:#{port}"}
-  
-  # exec instructions
-  instructions.each do |instruction|
-    puts instruction.join(" ") if debug
-    id, delay, cmd, args = instruction
+
+  abort("Error! No test recipe specified".red) unless recipe
+  abort("Error! Can't find recipe ".red) unless File.exists?(recipe)
+
+  # prepare log files
+  log_dir = File.join(FileUtils.pwd,'log/')
+  slog    = File.join(log_dir,File.basename(recipe,".*")+'-server.log');
+  clog    = File.join(log_dir,File.basename(recipe,".*")+'-client.log');
+  tlog    = File.join(log_dir,File.basename(recipe,".*")+'-test.log');
+
+  begin
+
+    fds << File.open(tlog,'w')
     
-    if ( id && delay && cmd)
-      sleep delay.to_f
-      cmdstr = mkcmd(cmd,args)
-      puts "#{id}".pink+"| #{Time.now.to_s} | "+"#{cmdstr}".yellow
-      clients[id.to_i].puts cmdstr
-      raise "Bad recipe" unless cmdstr
+    trap("INT"){ puts "Hold your horses".yellow; raise "SIGINT";}
+
+    # start a server
+    if not client_only
+      fd  =  File.open(slog,'w')
+      sread,sinput = IO.pipe
+      pids << Process.spawn(server, :err=>fd , :out=>fd, :in=>sread )
+      fds  << fd
+       
+      puts "Launching Local Server pid=#{pids.first}"
+      sleep 0.5
+      
+      puts "sh -c #{server}" if (debug) 
+      port_str = File.readlines("#{slog}")
+                     .join("\n").scan(/RPC Port: \d{4,5}/).first;
+      port = port_str.scan(/\d+/).first if port_str
+      raise "Error! Can't parse server log: #{port_str}".red unless port && port_str
     end
+   
+    # read the port
+    raise "Error! Can't find port".red unless port
+    puts "Server on #{ip}:#{port}"
+    
+    # read the recipe file
+    instructions = CSV.readlines(recipe);
+    count = instructions.shift.first.to_i;
+    
+    verbose = true if instructions.length < 10
+
+    # create clients
+    ios = count.times.map{ IO.pipe }
+    ios.each do |io|
+      fd = File.open(clog,'w');
+      pids << Process.spawn("#{client}",:in=>io.first,:err=>fd,:out=>fd)
+      fds << fd
+    end
+    puts "Clients #{pids[1..-1].inspect}"
+    clients = ios.map{|io| io.last}
+    
+    # connect all clients 
+    clients.each{|stdin| stdin.puts "connect #{ip}:#{port}"}
+    
+    # exec instructions
+    instructions.each do |instruction|
+      puts instruction.join(" ") if debug
+      id, delay, cmd, args = instruction
+      
+      if ( id && delay && cmd)
+        sleep delay.to_f
+        cmdstr = mkcmd(cmd,args)
+        log(id,cmdstr,verbose,fds.first)
+        clients[id.to_i].puts cmdstr
+        raise "Bad recipe" unless cmdstr
+      end
+    end
+
+    # issue dumps to errbody
+
+    # connect all clients 
+    clients.each{|stdin| stdin.puts "quit"}
+    
+    sinput.puts "quit" if sinput
+
+    # Wait for children to finish
+    Process.waitall
+  rescue 
+    puts "Error! Suspending Execution and killing children"
+    puts  $!, $@
+  ensure
+    puts "Cleaning Process & Closing FDs"
+
+    pids.each do |pid| 
+      begin 
+        Process.kill(15,pid); 
+      rescue Errno::ESRCH
+        nil
+      end
+    end
+    fds.each{|file| file.close }
   end
-
-  # issue dumps to errbody
-
-  # connect all clients 
-  clients.each{|stdin| stdin.puts "quit"}
-rescue 
-  puts "Error! Suspending Execution and killing children"
-  puts  $!, $@
-ensure
-  puts "Cleaning Process & Closing FDs"
-  pids.each{|pid| Process.kill(15,pid) }
-  fds.each{|file| file.close }
 end
+
+ARGV.select{|arg| arg == "console" }.any? ? console() : test()
