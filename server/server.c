a8710e6f2524b5eb5e6b51819371d86c604d30a4
Samir Ahmed <samirahmed2013@gmail.com>
2013-04-28 15:18:30 -0400

    implement toggle teleportation

--- server/server.c
+++ server/server.c
@@ -39,6 +39,7 @@
 
 static Maze maze; 		
 static char timestr[9];
+static int  teleport;
 //////////////////
 // HELPER CODE  //
 //////////////////
@@ -246,6 +247,8 @@ int action_handler( Proto_Session *s)
   proto_session_body_unmarshall_bytes(s, sizeof(Pos), sizeof(Pos), (char*)&next);
 
   rc = server_request_init(&maze,&request,fd,action,next.x,next.y);
+  request.test_mode = teleport; // allow server to enable teleport
+
   if (rc<0)
   {
     slog("ACT",&action,fd,&team,&id,rc);
@@ -268,6 +271,7 @@ int action_handler( Proto_Session *s)
 
 int init_game(void){
 	int rc;
+  teleport = 0;
   rc = maze_build_from_file(&maze,"./daGame.map");
   if (rc <0) fprintf(stderr, "ERROR: Failed to build map\n");
   
@@ -302,6 +306,17 @@ int docmd(char* cmd)
   if ((strncmp(cmd,"quit",sizeof("quit")-1))==0)
   {
     return -1;
+  }
+  else if ((strncmp(cmd,"teleport",sizeof("quit")-1))==0)
+  {
+    teleport = !teleport;
+    if (teleport) 
+      fprintf(stdout,"Teleportation " COLOR_BLUE "ENABLED" COLOR_END "\n");
+    else
+      fprintf(stdout,"Teleportation " COLOR_RED "DISABLED" COLOR_END "\n");
+
+    fflush(stdout);
+    return 0;
   }
   else if((strncmp(cmd,"textdump",sizeof("textdump")-1))==0) 
   { 
